16dcbd9907304e240c560c0a0579a8fd7f912f17
Kelsey Fecho <kfecho30@gmail.com>
2018-04-20 01:37:01 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,7 +39,7 @@ class SongsController < ApplicationController
     song = Song.find_by_slug(params[:slug])
     song.artist = Artist.find_or_create_by(:name => params["artist_name"])
     song.save
-
+    binding.pry
     flash[:message] = "Successfully updated song."
     redirect :"/songs/#{song.slug}"
   end
